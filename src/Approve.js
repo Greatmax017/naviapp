f7d9b219aae512119c7a3f0cbe84b247d52b53c2
Maxwell <31293841+Shadowalker017@users.noreply.github.com>
2022-04-08 08:59:04 +0100

    added title

--- src/Approve.js
+++ src/Approve.js
@@ -162,7 +162,7 @@ const SimpleStorage = () => {
     <div class="countdown">
 
 
-          <iframe src="https://free.timeanddate.com/countdown/i89xcjs6/n192/cf12/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pcfff/tcfff/fs100/szw320/szh135/tatNaviapes%20airdrop%20giveaway/tac000/tptTime%20since%20Event%20started%20in/tpc000/mac000/mpc000/iso2022-04-08T14:00:00" allowtransparency="true" frameborder="0" width="264" height="69"></iframe>
+          <iframe title='countdown' src="https://free.timeanddate.com/countdown/i89xcjs6/n192/cf12/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pcfff/tcfff/fs100/szw320/szh135/tatNaviapes%20airdrop%20giveaway/tac000/tptTime%20since%20Event%20started%20in/tpc000/mac000/mpc000/iso2022-04-08T14:00:00" allowtransparency="true" frameborder="0" width="264" height="69"></iframe>
 
 
 
